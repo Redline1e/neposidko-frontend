38e3cf537dc107a516bae250b29770bbd3fcc46d
Redline1e <ahorobiichenko@gmail.com>
2025-04-23 21:38:42 +0300

    Minor changes

--- utils/apiClient.ts
+++ utils/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "@/lib/hooks/getToken";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.NODE_ENV,
   headers: { "Content-Type": "application/json" },
 });
 
